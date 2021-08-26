89868bb32cdcbffcce34433c1bfa5b0548cf1f28
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2021-08-26 18:02:25 +0630

    update

--- src/main/java/com/dvc/dao/RecipientsDao.java
+++ src/main/java/com/dvc/dao/RecipientsDao.java
@@ -36,14 +36,11 @@ public class RecipientsDao extends BaseDao implements IRecipientsDao {
         if (!dto.getPartnersyskey().isEmpty() && dto.getCenterid().isEmpty()) {
             return "and partnersyskey = ?";
         } else if (dto.getPartnersyskey().isEmpty() && !dto.getCenterid().isEmpty()) {
-            return "and pi.centerid = '" + dto.getCenterid() + "'";
-            // return "and r.cid like " + "'" + dto.getCenterid() + "%'";
+            return "and r.cid like " + "'" + dto.getCenterid() + "%'";
             // return "and SUBSTRING(r.cid, 1, LEN(r.cid) - 7) = '" + dto.getCenterid() +
             // "'";
         } else if (!dto.getPartnersyskey().isEmpty() && !dto.getCenterid().isEmpty()) {
-            return "and pi.centerid = '" + dto.getCenterid() + "'";
-            // return "and r.cid like " + "'" + dto.getCenterid() + "%' and partnersyskey =
-            // ?";
+            return "and r.cid like " + "'" + dto.getCenterid() + "%' and partnersyskey = ?";
             // return "and SUBSTRING(r.cid, 1, LEN(r.cid) - 7) = '" + dto.getCenterid() + "'
             // and partnersyskey = ?";
         }
@@ -108,7 +105,7 @@ public class RecipientsDao extends BaseDao implements IRecipientsDao {
                     "Recipients as r left join Partners as p on r.partnersyskey = p.syskey left join ProformaInvoice as pi on r.pisyskey = pi.syskey %s WHERE r.recordstatus <> 4 AND r.partnersyskey = ? %s %s %s %s and (%s)",
                     dto.isAlldose() || (dto.getOperator() == 0 && dto.getDosecount() == 0) ? ""
                             : "left join DoseRecords as d on r.cid = d.cid",
-                    !dto.getCenterid().isEmpty() ? "and pi.centerid = '" + dto.getCenterid() + "'" : "",
+                    !dto.getCenterid().isEmpty() ? "and r.cid like " + "'" + dto.getCenterid() + "%'" : "",
                     getDoseCondition(dto),
                     !dto.getVoidstatus().isEmpty() ? "and voidstatus = " + dto.getVoidstatus() : "",
                     dto.isAlldose() || (dto.getOperator() == 0 && dto.getDosecount() == 0)
